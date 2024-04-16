9c3ffc6d25d239a0d77b98912aba2137c9d79a91
Neel Patel <148554877+neelpatel2@users.noreply.github.com>
2024-04-16 09:43:50 +0530

    Update program16.c

--- Module 3/Basic Logic Program/program16.c
+++ Module 3/Basic Logic Program/program16.c
@@ -1,58 +1,18 @@
-//Convert country’s name in abbreviate form
+//Convert country’s name in abbreviate form 
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h> 
-
-#define MAX_COUNTRY_LENGTH 100
-#define MAX_ABBREV_LENGTH 10
-
-typedef struct {
-    char name[MAX_COUNTRY_LENGTH];
-    char abbreviation[MAX_ABBREV_LENGTH];
-} Country;
-
-Country countries[] = {
-    {"United States of America", "US"},
-    {"United Kingdom", "UK"},
-    {"Canada", "CA"},
-    {"France", "FR"},
-    {"Germany", "DE"},
-    {"India", "IN"},
-    {"Sri Lanka", "SR"},
-    {"Australia", "AU"},
-    {"China", "CN"},
-    {"Pakistan", "Pk"},
-
-
-};
+#include <ctype.h>
 
 int main() {
-    char user_country[MAX_COUNTRY_LENGTH];
-    char abbreviation[MAX_ABBREV_LENGTH];
-
-    printf("Enter a country name (in uppercase or lowercase): ");
-    fgets(user_country, MAX_COUNTRY_LENGTH, stdin);
-
-    user_country[strcspn(user_country, "\n")] = '\0';
-
-    for (int i = 0; user_country[i] != '\0'; i++) {
-        user_country[i] = toupper(user_country[i]);
+    char str[100];
+    
+    printf("Enter a country name: ");
+    scanf("%s", str);
+    
+    for(int i = 0; i < 2; i++) {
+        str[i] = toupper(str[i]);
     }
-
-    abbreviation[0] = '\0'; 
-
-    for (int i = 0; i < sizeof(countries) / sizeof(countries[0]); i++) {
-        if (strcmp(user_country, countries[i].name) == 0) {
-            strcpy(abbreviation, countries[i].abbreviation);
-            break;
-        }
-    }
-
-    if (abbreviation[0] != '\0') {
-        printf("Abbreviation: %s\n", abbreviation);
-    } else {
-        printf("Abbreviation not found for '%s'\n", user_country);
-    }
-
+    
+    printf("Abbreviated form %c%c\n", str[0], str[1]);
+    
     return 0;
 }
