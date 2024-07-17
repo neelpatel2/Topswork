55e6b1cdd1fbed7ad961e2a2c203cb08213af22c
neelpatel2 <neelgami467@gmail.com>
2024-07-17 14:42:28 +0530

    update

--- Assignment/Python Assignment/Module 2/Q13.py
+++ Assignment/Python Assignment/Module 2/Q13.py
@@ -1,11 +1,12 @@
 #Write a Python program to count the number of characters (character frequency) in a string.
 
-string = input("Enter a string: ")
-frequency= {}
-for char in string:
-    if char in frequency:
-        frequency[char] += 1
-    else:
-        frequency[char] = 1
-for char, frequency in frequency.items():
-    print(f"The Character'{char}'appear: {frequency} times")
+name = input("Enter a something...")
+unique_ch = ""
+emp_ = dict()
+for ch in name:
+    if ch not in unique_ch:
+        unique_ch += ch
+        emp_.update({ch:name.count(ch)})
+print(emp_)
+
+print(unique_ch)
